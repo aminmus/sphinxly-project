1cabbd96d1f734e1152b9734b981cd489c1dc3a9
Amin Mushehid <amin.mushehid@chasacademy.se>
2019-07-03 19:47:12 +0200

    Add a basic form for visitors name and conditionally render visitors name.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php if (isset($_POST['visitor-name'])) : ?>
+    <h1>Välkommen <?php echo($_POST['visitor-name']) ?>!</h1>
+    <?php else : ?>
+    <h1>Hej!</h1>
+    <form action="index.php" method="POST">
+        <label>Vänligen skriv in ditt namn
+            <input type="text" name="visitor-name">
+        </label>
+    </form>
+    <?php endif ?>
+</body>
+
+</html>
