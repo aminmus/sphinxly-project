3fb362312b5a237452a8c208af67827b30756ce9
Amin Mushehid <amin.mushehid@chasacademy.se>
2019-07-15 00:55:50 +0200

    Fix capitalization of directories.

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -11,8 +11,8 @@ abstract class Controller
     public function view($view, $data = [])
     {
         // Check for view file
-        if (file_exists('../app/views/' . $view . '.php')) {
-            require_once '../app/views/' . $view . '.php';
+        if (file_exists('../App/Views/' . $view . '.php')) {
+            require_once '../App/Views/' . $view . '.php';
         } else {
             // View does not exist
             die('View does not exist');
